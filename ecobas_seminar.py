c30fae2784c080ff9d9bec95661732e3f43796c1
herves-pardavila <herves.david@gmail.com>
2024-07-17 20:40:45 +0200

    update token

---
+++ ecobas_seminar.py
@@ -0,0 +1,156 @@
+import pandas as pd
+from patsy import dmatrices
+import numpy as np
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import statsmodels.formula.api as smf
+import statsmodels.graphics as smg
+import time
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error 
+from datetime import datetime
+def ajuste(df,expr,name):
+    log_likelihood=[]
+    newdf=pd.DataFrame()
+    null_expr1="""Visitantes ~ 1 """
+    null_expr2="""Visitantes ~ Season + covid """
+    for park in df.IdOAPN.unique():
+        #print("===========================================")
+        #print("Park=",park)
+        #print("===========================================")
+        subdf=df[df.IdOAPN==park]
+
+        #divide between training set and test set
+        # np.random.seed(seed=1)
+        # mask=np.random.rand(len(subdf))<0.7
+        # df_train=subdf[mask]
+        df_train=subdf
+        # df_test=subdf[~mask]
+
+        y_train, X_train = dmatrices(expr, df_train, return_type='dataframe')
+        #y_test, X_test = dmatrices(expr, df_test, return_type='dataframe')
+        try:
+            poisson_training_results = sm.GLM(y_train, X_train, family=sm.families.Poisson()).fit()
+
+            #auxiliary regression model
+            df_train['BB_LAMBDA'] = poisson_training_results.mu
+            df_train['AUX_OLS_DEP'] = df_train.apply(lambda x: ((x['Visitantes'] - x['BB_LAMBDA'])**2 - x['BB_LAMBDA']) / x['BB_LAMBDA'], axis=1)
+            ols_expr = """AUX_OLS_DEP ~ BB_LAMBDA - 1"""
+            aux_olsr_results = smf.ols(ols_expr, df_train).fit()
+
+            #negative_binomial regression
+            #print("=========================Negative Binomial Regression===================== ")
+            nb2_training_results = sm.GLM(y_train, X_train,family=sm.families.NegativeBinomial(alpha=aux_olsr_results.params[0])).fit()
+            #predicitons
+            nb2_predictions = nb2_training_results.get_prediction(X_train)
+            predictions_summary_frame = nb2_predictions.summary_frame()
+            df_train["yhat"+name]=predicted_counts=predictions_summary_frame['mean']
+
+            #negative binomial regression with intercept only to compute pseudo R2 using deviance
+            y_train, X_train = dmatrices(null_expr1, df_train, return_type='dataframe')
+            nb2_intercept_only= sm.GLM(y_train, X_train,family=sm.families.NegativeBinomial(alpha= aux_olsr_results.params[0] )).fit()
+            df_train["R2dev1"+name]=1-(nb2_training_results.deviance/nb2_intercept_only.deviance)
+            y_train, X_train = dmatrices(null_expr2, df_train, return_type='dataframe')
+            nb2_intercept_only= sm.GLM(y_train, X_train,family=sm.families.NegativeBinomial(alpha= aux_olsr_results.params[0] )).fit()
+            df_train["R2dev2"+name]=1-(nb2_training_results.deviance/nb2_intercept_only.deviance)
+  
+            #df_test.loc[df_test.index,"R2yhat"+name]=r2
+            #df_test.loc[df_test.index,"corryhat"+name]=df_test[["yhat"+name,name]].corr(method="spearman").loc["yhat"+name][name]
+            log_likelihood+=[nb2_training_results.llf]
+            newdf=pd.concat([newdf,df_train])
+
+        except ValueError:
+                continue
+    log_likelihood=np.array(log_likelihood)
+    print("Suma de los log-likelihood=",np.sum(log_likelihood))
+    newdf=newdf[["Date","IdOAPN","Visitantes"]+["yhat"+name]+["R2dev1"+name]+["R2dev2"+name]]
+    newdf=newdf.round({"R2dev1"+name:2,"R2dev2"+name:2})
+    return newdf
+if __name__== "__main__":
+
+    #prepare the data
+    dataframe=pd.read_csv("/home/usuario/Documentos/recreation/recreation_ready.csv")
+    dataframe.Date=dataframe.Date.astype("category")
+    dataframe.Month=dataframe.Month.astype("category")
+    dataframe.Year=dataframe.Year.astype("category")
+    dataframe.IdOAPN=dataframe.IdOAPN.astype("category")
+    dataframe.Season=dataframe.Season.astype("category")
+    dataframe.covid=dataframe.covid.astype("category")
+    dataframe.Visitantes=dataframe.Visitantes.astype(int)
+    print(dataframe.info()) 
+    
+    #model 1
+    expr1="""Visitantes ~ logPUD + Season + covid"""
+    df1=dataframe.dropna(subset=["Visitantes","PUD"])
+    newdf1=ajuste(df1,expr1,"model1")
+    
+    #model2
+    expr2="""Visitantes ~ turistas_total+ Season + covid"""
+    df2=dataframe.dropna(subset=["Visitantes","turistas_total"])
+    df2.turistas_total=df2.turistas_total.astype(int)
+    newdf2=ajuste(df2,expr2,"model2")
+    
+    #model4
+    expr4="""Visitantes ~ logPUD + logIUD + Season + covid"""
+    df4=dataframe.dropna(subset=["Visitantes","IUD","PUD"])
+    df4["logIUD"]=np.log(df4.IUD+1)
+    newdf4=ajuste(df4,expr4,"model4")
+
+    #model 5
+    expr5="""Visitantes ~ logPUD + turistas_total+ Season + covid"""
+    df5=dataframe.dropna(subset=["Visitantes","turistas_total","PUD"])
+    df5.turistas_total=df5.turistas_total.astype(int)
+    newdf5=ajuste(df5,expr5,"model5")
+   
+
+   
+    
+    newdf=pd.merge(newdf1,newdf2,on=["Date","IdOAPN","Visitantes"],how="outer")
+    newdf=pd.merge(newdf,newdf4,on=["Date","IdOAPN","Visitantes"],how="outer")
+    newdf=pd.merge(newdf,newdf5,on=["Date","IdOAPN","Visitantes"],how="outer")
+    print(newdf)
+
+    newdfA=newdf[newdf.IdOAPN=="Aigüestortes i Estany de Sant Maurici"]
+    newdfB=newdf[newdf.IdOAPN=="Archipiélago de Cabrera"]
+    newdfC=newdf[newdf.IdOAPN=="Cabañeros"]
+    newdfD=newdf[newdf.IdOAPN=="Caldera de Taburiente"]
+    newdfE=newdf[newdf.IdOAPN=="Doñana"]
+    newdfF=newdf[newdf.IdOAPN=="Garajonay"]
+    newdfG=newdf[newdf.IdOAPN=="Islas Atlánticas de Galicia"]
+    newdfH=newdf[newdf.IdOAPN=="Monfragüe"]
+    newdfI=newdf[newdf.IdOAPN=="Ordesa y Monte Perdido"]
+    newdfJ=newdf[newdf.IdOAPN=="Picos de Europa"]
+    newdfK=newdf[newdf.IdOAPN=="Sierra Nevada"]
+    newdfL=newdf[newdf.IdOAPN=="Sierra de Guadarrama"]
+    newdfM=newdf[newdf.IdOAPN=="Sierra de las Nieves"]
+    newdfN=newdf[newdf.IdOAPN=="Tablas de Daimiel"]
+    newdfO=newdf[newdf.IdOAPN=="Teide National Park"]
+    newdfP=newdf[newdf.IdOAPN=="Timanfaya"]
+
+    fig2=plt.figure()
+    ax2=fig2.add_subplot(111)
+
+    #fig2.subplots_adjust(hspace=0.45,left=0.075,right=0.95, top=0.9, bottom=0.075)
+    fig2.text(x=0.40,y=0.025,s="Observed Visitors",fontsize=15)
+    fig2.text(x=0.025,y=0.40,s="Estimated Visitors",rotation="vertical",fontsize=15)
+    newdfA=newdfK
+    ax2.loglog(newdfA.Visitantes,newdfA.yhatmodel1,"o",color="pink",label="Flickr")
+    ax2.loglog(newdfA.Visitantes,newdfA.yhatmodel2,"o",color="red",label="Phones")
+    ax2.loglog(newdfA.Visitantes,newdfA.yhatmodel4,"o",color="blue",label="Flickr+Instagram")
+    ax2.loglog(newdfA.Visitantes,newdfA.yhatmodel5,"o",color="black",label="Flickr+Phones")
+    ax2.plot(np.linspace(min(newdfA.Visitantes),max(newdfA.Visitantes),100),np.linspace(min(newdfA.Visitantes),max(newdfA.Visitantes),100),label="1-1 line")
+    ax2.set_title(newdfA.IdOAPN.unique()[0],fontsize=12)
+    ax2.text(0.75,0.35,str(newdfA.R2dev1model1.min()),transform=ax2.transAxes,color="pink",fontsize=15)
+    ax2.text(0.75,0.25,str(newdfA.R2dev1model2.min()),transform=ax2.transAxes,color="red",fontsize=15)
+    #ax2.text(0.75,0.15,str(newdfA.R2dev1model4.min()),transform=ax2.transAxes,color="blue",fontsize=15)#
+    ax2.text(0.75,0.05,str(newdfA.R2dev1model5.min()),transform=ax2.transAxes,color="black",fontsize=15)
+    ax2.text(0.85,0.35,"("+str(newdfA.R2dev2model1.min())+")",transform=ax2.transAxes,color="pink",fontsize=15)
+    ax2.text(0.85,0.25,"(" + str(newdfA.R2dev2model2.min())+")",transform=ax2.transAxes,color="red",fontsize=15)
+    #ax2.text(0.85,0.15,"("+str(newdfA.R2dev2model4.min())+")",transform=ax2.transAxes,color="blue",fontsize=15)#
+    ax2.text(0.85,0.05,"("+str(newdfA.R2dev2model5.min())+")",transform=ax2.transAxes,color="black",fontsize=15)
+    ax2.tick_params(axis='both',labelsize=15)
+    fig2.legend(loc="upper center", ncols=5, fontsize=10,mode="expand")
+    plt.show()
+    fig2.savefig("/home/usuario/Documentos/recreation/imagenes_paper/"+newdfA.IdOAPN.unique()[0]+".png")
+
+    
--- pooled_regression.py
+++ pooled_regression.py
@@ -12,7 +12,8 @@ if __name__== "__main__":
 
     #prepare the data
     df=pd.read_csv("/home/usuario/Documentos/recreation/recreation_ready.csv")
-    #df=df[df.IdOAPN.isin(["Timanfaya","Islas Atlánticas de Galicia","Tablas de Daimiel"])]
+    df=df[df.Visitantes != 0]
+    df=df[df.IdOAPN.isin(["Timanfaya","Islas Atlánticas de Galicia","Tablas de Daimiel","Monfragüe","Cabañeros","Archipiélago de Cabrera"])]
     #df=df[df.Year.isin([2015,2016,2017,2018])]
     df.Date=df.Date.astype("category")
     df.Month=df.Month.astype("category")
@@ -22,34 +23,34 @@ if __name__== "__main__":
     df.covid=df.covid.astype("category")
 
 
-    # print(df)
-    # print(df.info())
-    # sum_statistics=df[["Visitantes","PUD","IUD","turistas_total","turistas_corregido"]].describe()
-    # sum_statistics=sum_statistics.round({"Visitantes":0,"PUD":1,"IUD":1,"turistas_total":0,"turistas_corregido":0})
-    # print(sum_statistics)
-    # sum_statistics.to_csv("/home/usuario/Documentos/recreation/imagenes_paper/summary_statistics.csv",index=True)
+    print(df)
+    print(df.info())
+    sum_statistics=df[["Visitantes","PUD","IUD","turistas_total","turistas_corregido"]].describe()
+    sum_statistics=sum_statistics.round({"Visitantes":0,"PUD":1,"IUD":1,"turistas_total":0,"turistas_corregido":0})
+    print(sum_statistics)
+    sum_statistics.to_csv("/home/usuario/Documentos/recreation/imagenes_paper/summary_statistics.csv",index=True)
 
-    #print(df[["Visitantes","PUD","IUD","turistas_total","turistas_corregido"]].corr("spearman"))
+    print(df[["Visitantes","PUD","IUD","turistas_total","turistas_corregido"]].corr("spearman"))
 
-    # dfmean=df[["IdOAPN","Visitantes","PUD","IUD","turistas_total","turistas_corregido","Month"]].groupby(by=["IdOAPN","Month"],as_index=False).mean()
-    # dfvar=df[["IdOAPN","Visitantes","Month"]].groupby(by=["IdOAPN","Month"],as_index=False).var()
-    # dfmean=dfmean.merge(dfvar,on=["IdOAPN","Month"],how="inner")
-    # dfmean["mu/sigma"]=dfmean.Visitantes_x/dfmean.Visitantes_y
-    # print(dfmean)
-    # print(dfmean.describe())
-    # dfmean=dfmean.groupby(by="IdOAPN",as_index=False).mean(numeric_only=True)
-    # dfmean.rename(columns={"Visitantes_x":"Visitors (mu)","PUD":"FUD","turistas_total":"MPUD","turistas_corregido":"correctedMPUD"},inplace=True)
-    # dfmean=dfmean.round({"Visitors (mu)":0,"FUD":1,"IUD":1,"MPUD":0,"correctedMPUD":0,"mu/sigma":4})
-    # dfmean.sort_values(by="Visitors (mu)",inplace=True,ascending=False)
-    # print(dfmean)
-    # dfmean.to_csv("/home/usuario/Documentos/recreation/imagenes_paper/tabla_overdispersion.csv",index=False)
+    dfmean=df[["IdOAPN","Visitantes","PUD","IUD","turistas_total","turistas_corregido","Month"]].groupby(by=["IdOAPN","Month"],as_index=False).mean()
+    dfvar=df[["IdOAPN","Visitantes","Month"]].groupby(by=["IdOAPN","Month"],as_index=False).var()
+    dfmean=dfmean.merge(dfvar,on=["IdOAPN","Month"],how="inner")
+    dfmean["mu/sigma"]=dfmean.Visitantes_x/dfmean.Visitantes_y
+    print(dfmean)
+    print(dfmean.describe())
+    dfmean=dfmean.groupby(by="IdOAPN",as_index=False).mean(numeric_only=True)
+    dfmean.rename(columns={"Visitantes_x":"Visitors (mu)","PUD":"FUD","turistas_total":"MPUD","turistas_corregido":"correctedMPUD"},inplace=True)
+    dfmean=dfmean.round({"Visitors (mu)":0,"FUD":1,"IUD":1,"MPUD":0,"correctedMPUD":0,"mu/sigma":4})
+    dfmean.sort_values(by="Visitors (mu)",inplace=True,ascending=False)
+    print(dfmean)
+    dfmean.to_csv("/home/usuario/Documentos/recreation/imagenes_paper/tabla_overdispersion.csv",index=False)
 
     
 
     #divide between training set and test set
-    df.dropna(subset=["Visitantes","IUD","PUD"],inplace=True)
+    df.dropna(subset=["Visitantes","IUD"],inplace=True)
     df.Visitantes=df.Visitantes.astype(int)
-    df.turistas_total=df.turistas_total.astype(int)
+    #df.turistas_total=df.turistas_total.astype(int)
     #df["log_Summer_turistas_corregido"]=np.log(df.Summer_turistas_corregido+1)
     df["logIUD"]=np.log(df.IUD+1)
     np.random.seed(seed=1)
@@ -60,13 +61,13 @@ if __name__== "__main__":
 
 
     #poisson model
-    expr1="""Visitantes ~ IdOAPN + Season + covid"""
+    expr1="""Visitantes ~ logPUD + IdOAPN + Season + covid"""
     expr2="""Visitantes ~ turistas_total+ IdOAPN + Season + covid"""
     expr3="""Visitantes ~ logIUD + IdOAPN + Season + covid"""
     expr4="""Visitantes ~ logPUD + logIUD + IdOAPN + Season + covid"""
     expr5="""Visitantes ~ logPUD + turistas_total+ IdOAPN + Season + covid"""
     null_expr="""Visitantes ~ IdOAPN + Season + covid """
-    expr=expr4
+    expr=expr3
     y_train, X_train = dmatrices(expr, df_train, return_type='dataframe')
     #y_test, X_test = dmatrices(expr, df_test, return_type='dataframe')
     poisson_training_results = sm.GLM(y_train, X_train, family=sm.families.Poisson()).fit()
@@ -93,24 +94,24 @@ if __name__== "__main__":
     summary_as_html=summary.tables[1].as_html()
     summary_as_df=pd.read_html(summary_as_html,header=0,index_col=0)[0]
     summary_as_df.loc["DF","coef"]=int(nb2_training_results.df_resid)
-    summary_as_df.loc["Log-likelihood","coef"]=int(nb2_training_results.llf)
+    #summary_as_df.loc["Log-likelihood","coef"]=int(nb2_training_results.llf)
     #summary_as_df.loc["Deviance","coef"]=int(nb2_training_results.deviance)
     summary_as_df.loc["chi2","coef"]=int(nb2_training_results.pearson_chi2)
     #summary_as_df.loc["AIC","coef"]=int(nb2_training_results.aic)
  
-    #average marginal effects
-    betas=nb2_training_results.params
-    Z=X_train.dot(betas)
-    ey=np.exp(Z)
-    print("AME of variable %s ="%str(betas.index[-1]),betas.iloc[-1]*ey.mean())
-    print("AME of variable %s ="%str(betas.index[-2]),betas.iloc[-2]*ey.mean())
+    # #average marginal effects
+    # betas=nb2_training_results.params
+    # Z=X_train.dot(betas)
+    # ey=np.exp(Z)
+    # print("AME of variable %s ="%str(betas.index[-1]),betas.iloc[-1]*ey.mean())
+    # print("AME of variable %s ="%str(betas.index[-2]),betas.iloc[-2]*ey.mean())
 
     #negative binomial regression with intercept only to compute pseudo R2 using deviance
     y_train, X_train = dmatrices(null_expr, df_train, return_type='dataframe')
     nb2_intercept_only= sm.GLM(y_train, X_train,family=sm.families.NegativeBinomial(alpha= aux_olsr_results.params[0] )).fit()
     summary_as_df.loc["R2dev","coef"]=1-(nb2_training_results.deviance/nb2_intercept_only.deviance)
     print(summary_as_df)
-    #summary_as_df.to_csv("/home/usuario/Documentos/recreation/imagenes_paper/tabla_modelo1.csv")
+    #summary_as_df.to_csv("/home/usuario/Documentos/recreation/imagenes_paper/tabla_modelo4.csv")
 
 
 
