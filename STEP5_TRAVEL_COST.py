74eaaa8e8e904bf56b6a76525c9b8c62cd1dc594
David Hervés Pardavila <121116532+herves-pardavila@users.noreply.github.com>
2025-09-17 17:33:16 +0200

    stupid commit

--- STEP5_TRAVEL_COST.py
+++ STEP5_TRAVEL_COST.py
@@ -94,8 +94,8 @@ if __name__== "__main__":
     print("La sobredispersion es del",df.Y.mean()/df.Y.std())
     
     #poisson model
-    model="log-lin"
-    expr="""y~TC + median_inc"""
+    model="log-log"
+    expr="""y~lnTC + lnI"""
     null_expr="Y~1"
   
     y_train, X_train = dmatrices(expr, df_train, return_type='dataframe')
