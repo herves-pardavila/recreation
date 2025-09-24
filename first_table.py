ed63bd167996434ed651a254ca934fac9df725d3
David Hervés Pardavila <121116532+herves-pardavila@users.noreply.github.com>
2025-09-24 13:12:47 +0200

    Update data source and region, fix percentage calculations
    Changed the input CSV to '3travel_cost_Aiguestortes_ready.csv' and updated the region to 'Cataluña'. Adjusted calculation of 'Q_resto' to sum both 'Mundo' and 'Europa' zones. Replaced np.round with float division for percentage outputs and fixed a typo in 'Datos moviles'.

--- first_table.py
+++ first_table.py
@@ -13,26 +13,26 @@ if __name__ == "__main__":
     path=r"F:/doctorado/"
     #load the data
     #df=pd.read_csv(path+"3travel_cost_Ons.csv")
-    df=pd.read_csv(path+"recreation/ZonalTravelCost/3travel_cost_Ons_ready.csv")
+    df=pd.read_csv(path+"recreation/ZonalTravelCost/3travel_cost_Aiguestortes_ready.csv")
     
     df=df[df.Año.isin([2022,2023])]
     df=df.groupby(["Lugar","Zona"],as_index=False).mean(numeric_only=True)
     
-    comunidad_autonoma="Galicia"
+    comunidad_autonoma="Cataluña"
     
     Q_comunidad=df[df.Lugar == comunidad_autonoma][["Numero","turistasINE"]]
     Q_españa=df.groupby(by="Zona",as_index=True).sum(numeric_only=True).loc["España",["Numero","turistasINE"]]
-    Q_resto=df.groupby(by="Zona",as_index=True).sum(numeric_only=True).loc["Europa",["Numero","turistasINE"]]
+    Q_resto=df.groupby(by="Zona",as_index=True).sum(numeric_only=True).loc[["Mundo","Europa"],["Numero","turistasINE"]].sum()
     
     print("Datos on-site")
     var="Numero"
-    print("Porcentaje de visitantes de la misma comunidad=",np.round(Q_comunidad[var]/(Q_españa[var]+Q_resto[var]),4))
-    print("Porcentaje de visitantes de otra comunidad=",np.round((Q_españa[var]-Q_comunidad[var])/(Q_españa[var]+Q_resto[var]),4))
-    print("Porcentaje de visitantes no españoles=",np.round(Q_resto[var]/(Q_españa[var]+Q_resto[var]),4))
+    print("Porcentaje de visitantes de la misma comunidad=",float(Q_comunidad[var])/(float(Q_españa[var])+float(Q_resto[var])))
+    print("Porcentaje de visitantes de otra comunidad=",float(Q_españa[var]-Q_comunidad[var])/float(Q_españa[var]+Q_resto[var]))
+    print("Porcentaje de visitantes no españoles=",float(Q_resto[var])/float(Q_españa[var]+Q_resto[var]))
     
     
-    print("Datos mobiles")
+    print("Datos moviles")
     var="turistasINE"
-    print("Porcentaje de visitantes de la misma comunidad=",np.round(Q_comunidad[var]/(Q_españa[var]+Q_resto[var]),4))
-    print("Porcentaje de visitantes de otra comunidad=",np.round((Q_españa[var]-Q_comunidad[var])/(Q_españa[var]+Q_resto[var]),4))
-    print("Porcentaje de visitantes no españoles=",np.round(Q_resto[var]/(Q_españa[var]+Q_resto[var]),4))
+    print("Porcentaje de visitantes de la misma comunidad=",float(Q_comunidad[var])/(float(Q_españa[var])+float(Q_resto[var])))
+    print("Porcentaje de visitantes de otra comunidad=",float(Q_españa[var]-Q_comunidad[var])/float(Q_españa[var]+Q_resto[var]))
+    print("Porcentaje de visitantes no españoles=",float(Q_resto[var])/float(Q_españa[var]+Q_resto[var]))
