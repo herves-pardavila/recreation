3b3089434865f29ec4af6cc82714fb7a3cba158b
David Hervés Pardavila <121116532+herves-pardavila@users.noreply.github.com>
2025-10-03 09:15:31 +0200

    Filter data by Zona and update model expression
    Data is now filtered to include only rows where Zona is 'España' and turistasINE > 0. The Poisson model is changed from a log-linear to a log-log specification, updating the formula to use lnTC and lnI.

--- STEP4_TRAVEL_COST.py
+++ STEP4_TRAVEL_COST.py
@@ -28,7 +28,7 @@ if __name__== "__main__":
     #============= ELEGIR BIEN LOS AÑOS==========
     df=df[df.Año.isin([2022,2023])] #Ons y Aiguestortes son 2022 y 2023, Cabañeros solo 2022
     df=df.groupby(["Lugar","Zona","Población","RBMPP","TC"],as_index=False).sum(numeric_only=True)
-    df=df.loc[df.turistasINE>0]
+    df=df.loc[(df.turistasINE>0) & (df.Zona=="España")]
     #remove nans
     
 
@@ -98,8 +98,8 @@ if __name__== "__main__":
     print("La sobredispersion es del",df.Y.mean()/df.Y.std())
     
     #poisson model
-    model="log-lin"
-    expr="""y ~ TC + Income + Zona"""
+    model="log-log"
+    expr="""y ~ lnTC + lnI """
     null_expr="Y~1"
   
     y_train, X_train = dmatrices(expr, df_train, return_type='dataframe')
